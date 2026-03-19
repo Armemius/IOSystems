bb2a87e6f9ce1a7e453ecd7dea1d4bd42eb2015c
Armemius <rusgy@yandex.ru>
2026-03-19 20:33:01 +0300

    feat: provide basic project layout

---
+++ lab-1/src/kernel.c
@@ -0,0 +1,16 @@
+#include "kernel.h"
+
+extern char __bss[], __bss_end[], __stack_top[];
+
+void kernel_main(void) {
+  for (;;) {
+    __asm__ __volatile__("wfi");
+  }
+}
+
+__attribute__((section(".text.boot"))) __attribute__((naked)) void boot(void) {
+  __asm__ __volatile__("mv sp, %[stack_top]\n"
+                       "j kernel_main\n"
+                       :
+                       : [stack_top] "r"(__stack_top));
+}
